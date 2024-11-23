e53263576d9bb90d2feacdc5e1e3cca91ae81887
Fernando Figueira <690686+fmarquesfilho@users.noreply.github.com>
2024-11-23 18:19:05 -0300

    add debug code

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    sourcemap: true, // Enable source maps for build
+  },
   resolve: {
     alias: {
       $lib: '/src/lib',  // This will create an alias for the src/lib folder
